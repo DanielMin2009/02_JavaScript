bffabd8ca4ba089a74d850a33c6a4a7e214a84e4
DanielMin2009 <daniel.minguella.blasco@gmail.com>
2020-02-25 13:11:09 +0100

    repair modal and styles

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,86 @@
-/* ======================== count Words 01 ======================== */
-document.getElementById('btn-modalAdd').addEventListener('click', function () {
-    document.getElementById('overlayAdd').classList.add('is-visible');
-    document.getElementById('modalAdd').classList.add('is-visible');
+/* ======================== count Words | 01 ======================== */
+document.getElementById("btn-modalAdd").addEventListener("click", function () {
+    document.getElementById("overlayAdd").classList.add("is-visible");
+    document.getElementById("modalAdd").classList.add("is-visible");
 });
 
-document.getElementById('close-btnAdd').addEventListener('click', function () {
-    document.getElementById('overlayAdd').classList.remove('is-visible');
-    document.getElementById('modalAdd').classList.remove('is-visible');
+document.getElementById("close-btnAdd").addEventListener("click", function () {
+    document.getElementById("overlayAdd").classList.remove("is-visible");
+    document.getElementById("modalAdd").classList.remove("is-visible");
 });
 
-document.getElementById('splitName').addEventListener('click', function () {
-    document.getElementById('overlayAdd').classList.remove('is-visible');
-    document.getElementById('modalAdd').classList.remove('is-visible');
+document.getElementById("btnSplitName").addEventListener("click", function () {
+    document.getElementById("overlayAdd").classList.remove("is-visible");
+    document.getElementById("modalAdd").classList.remove("is-visible");
 });
 
-document.getElementById('overlayAdd').addEventListener('click', function () {
-    document.getElementById('overlayAdd').classList.remove('is-visible');
-    document.getElementById('modalAdd').classList.remove('is-visible');
+document.getElementById("overlayAdd").addEventListener("click", function () {
+    document.getElementById("overlayAdd").classList.remove("is-visible");
+    document.getElementById("modalAdd").classList.remove("is-visible");
+});
+
+
+/* ======================== find Vocals | 02 ======================== */
+document.getElementById("btn-modal02").addEventListener("click", function () {
+    document.getElementById("overlay02").classList.add("is-visible");
+    document.getElementById("modal02").classList.add("is-visible");
+});
+
+document.getElementById("close-btn02").addEventListener("click", function () {
+    document.getElementById("overlay02").classList.remove("is-visible");
+    document.getElementById("modal02").classList.remove("is-visible");
+});
+
+document.getElementById("btnFindVocals").addEventListener("click", function () {
+    document.getElementById("overlay02").classList.remove("is-visible");
+    document.getElementById("modal02").classList.remove("is-visible");
+});
+
+document.getElementById("overlay02").addEventListener("click", function () {
+    document.getElementById("overlay02").classList.remove("is-visible");
+    document.getElementById("modal02").classList.remove("is-visible");
+});
+
+
+/* ======================== map Name | 03 ======================== */
+document.getElementById("btn-modal03").addEventListener("click", function () {
+    document.getElementById("overlay03").classList.add("is-visible");
+    document.getElementById("modal03").classList.add("is-visible");
+});
+
+document.getElementById("close-btn03").addEventListener("click", function () {
+    document.getElementById("overlay03").classList.remove("is-visible");
+    document.getElementById("modal03").classList.remove("is-visible");
+});
+
+document.getElementById("btnMapName").addEventListener("click", function () {
+    document.getElementById("overlay03").classList.remove("is-visible");
+    document.getElementById("modal03").classList.remove("is-visible");
+});
+
+document.getElementById("overlay03").addEventListener("click", function () {
+    document.getElementById("overlay03").classList.remove("is-visible");
+    document.getElementById("modal03").classList.remove("is-visible");
+});
+
+
+/* ======================== Name Surname Positions | 04 ======================== */
+document.getElementById("btn-modal04").addEventListener("click", function () {
+    document.getElementById("overlay04").classList.add("is-visible");
+    document.getElementById("modal04").classList.add("is-visible");
+});
+
+document.getElementById("close-btn04").addEventListener("click", function () {
+    document.getElementById("overlay04").classList.remove("is-visible");
+    document.getElementById("modal04").classList.remove("is-visible");
+});
+
+document.getElementById("btnNameSurname").addEventListener("click", function () {
+    document.getElementById("overlay04").classList.remove("is-visible");
+    document.getElementById("modal04").classList.remove("is-visible");
+});
+
+document.getElementById("overlay04").addEventListener("click", function () {
+    document.getElementById("overlay04").classList.remove("is-visible");
+    document.getElementById("modal04").classList.remove("is-visible");
 });
